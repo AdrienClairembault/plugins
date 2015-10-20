4cce8696df59c775eb47b45de8fdc9d8db3d2e10
Nelson Zamith <nelson@zamith.net>
2015-10-20 23:39:51 +0200

    first draft for scrolling mode

--- frontend/app/scripts/controllers/nav.js
+++ frontend/app/scripts/controllers/nav.js
@@ -8,8 +8,8 @@
  * Controller of the frontendApp
  */
 angular.module('frontendApp')
-   .controller('NavCtrl', ['$scope', '$mdSidenav', '$rootScope',
-      function($scope, $mdSidenav, $rootScope) {
+   .controller('NavCtrl', ['$scope', '$mdSidenav', '$rootScope', '$state',
+      function($scope, $mdSidenav, $rootScope, $state) {
 
          $scope.toggleNavBar = function() {
             $mdSidenav("side-menu").toggle();
@@ -21,6 +21,31 @@ angular.module('frontendApp')
 
          $rootScope.$on('$stateChangeStart', function() {
             $scope.closeNavBar();
+         });
+
+         $scope.$on('$stateChangeStart', function(event, toState, toParams) {
+            var jqueryWindow = $(window);
+            var sidebar = $('.sidebar');
+            var main = $('.main');
+            
+            if (toState.name == 'developerguides') {
+               jqueryWindow.scroll(function() {
+                  var h = jqueryWindow.scrollTop();
+                  if (h > 370 && !sidebar.hasClass('scrolling-mode')) {
+                     sidebar.addClass('scrolling-mode');
+                     main.addClass('scrolling-mode');
+                  } else if (h <= 370 && sidebar.hasClass('scrolling-mode')) {
+                     sidebar.removeClass('scrolling-mode')
+                     main.removeClass('scrolling-mode');
+                  }
+               });
+            } else {
+               jqueryWindow.off('scroll');
+               if (sidebar.hasClass('scrolling-mode')) {
+                  sidebar.removeClass('scrolling-mode');
+                  main.removeClass('scrolling-mode');
+               }
+            }
          });
       }
    ]);
