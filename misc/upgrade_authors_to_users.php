557adb0bb6b31ea10bc717c3489ee73a65140e5c
Nelson Zamith <zamith.nelson@gmail.com>
2015-12-06 23:42:16 +0100

    migration

--- misc/upgrade_authors_to_users.php
+++ misc/upgrade_authors_to_users.php
@@ -6,3 +6,17 @@ use Illuminate\Database\Capsule\Manager as DB;
 
 \API\Core\DB::initCapsule();
 
+$authors = DB::table('author')->get();
+
+foreach ($authors as $author) {
+	$fromAuthor = false;
+	$user = DB::table('user')->where('author_id', '=', $author->id)->first();
+	if (!$user) {
+		// $user = $DB::table('user')->insert([
+		// 	'realname' => $author->name
+		// ]);
+		$fromAuthor = true;
+	}
+
+	
+}
